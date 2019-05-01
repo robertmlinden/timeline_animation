1b1f031cfeb18baaf91d1bfda8e754d21e9788d9
Robert <rlinden@g.hmc.edu>
2019-05-01 16:44:20 -0700

    Zoom's a bit off

--- go.py
+++ go.py
@@ -8,7 +8,7 @@ class Timeline():
 		self.canvas = tk.Canvas(self.app, bg="black", height=c.HEIGHT, width=c.WIDTH)
 		self.canvas.pack()
 		self.start_x, self.end_x = c.START_X, c.END_X
-		self.pre_zoom_start_x, self.pre_zoom_end_x = self.start_x, self.end_x
+		self.zoom_factor = 1
 
 	def go(self):
 		self.app.mainloop()
@@ -24,22 +24,20 @@ class Timeline():
 		tick_distance = self.__length() / c.num_ticks()
 		return self.start_x + int((tick_num - c.START_TICK) * tick_distance)
 
-	def __zoom_factor(self):
-		pre_zoom_length = self.pre_zoom_end_x - self.pre_zoom_start_x
-		zoom_factor = self.__length() / pre_zoom_length
-		return zoom_factor
+	def __set_zoom_factor(self):
+		self.zoom_factor = self.__length() / c.LENGTH
 
 	def display_line(self):
-		zoom_factor = self.__zoom_factor()
+		self.__set_zoom_factor()
 
-		self.canvas.create_line(self.start_x, c.Y, self.end_x, c.Y,  fill=c.FILL_COLOR, width=zoom_factor)
+		self.canvas.create_line(self.start_x, c.Y, self.end_x, c.Y,  fill=c.FILL_COLOR, width=self.zoom_factor)
 
 		for tick_num in c.TICKS:
 			x = self.__tick_x(tick_num)
 			y_start = c.Y
-			y_end = y_start - (c.TICK_HEIGHT * zoom_factor)
+			y_end = y_start - (c.TICK_HEIGHT * self.zoom_factor)
 
-			self.canvas.create_line(x, y_start, x, y_end, fill=c.FILL_COLOR, width = zoom_factor)
+			self.canvas.create_line(x, y_start, x, y_end, fill=c.FILL_COLOR, width = self.zoom_factor)
 
 		self.app.update()
 
@@ -50,9 +48,6 @@ class Timeline():
 		incr_start_x = (goal_start_x - self.start_x) / c.ZOOM_SMOOTHNESS
 		incr_end_x = (goal_end_x - self.end_x) / c.ZOOM_SMOOTHNESS
 
-		self.pre_zoom_start_x = self.start_x
-		self.pre_zoom_end_x = self.end_x
-
 		for idx in range(c.ZOOM_SMOOTHNESS):
 			self.start_x = self.start_x - incr_start_x
 			self.end_x = self.end_x - incr_end_x
@@ -62,5 +57,6 @@ class Timeline():
 
 timeline = Timeline()
 timeline.display_line()
-timeline.zoom(20, 30)
+timeline.zoom(0, 10)
+timeline.zoom(30, 40)
 timeline.go()
