25b3f1d24b4e6cc5c1bf2a768b14eb51e2b9388c
Robert <rlinden@g.hmc.edu>
2019-05-01 18:23:49 -0700

    Looking decent

--- go.py
+++ go.py
@@ -7,8 +7,9 @@ class Timeline():
 		self.app = tk.Tk()
 		self.canvas = tk.Canvas(self.app, bg="black", height=c.HEIGHT, width=c.WIDTH)
 		self.canvas.pack()
-		self.start_x, self.end_x = c.START_X, c.END_X
-		self.zoom_factor = 1
+		self.__start_x, self.__end_x = c.START_X, c.END_X
+		self.__zoom_factor = 1
+		self.__start_tick = c.START_TICK
 
 	def go(self):
 		self.app.mainloop()
@@ -18,45 +19,68 @@ class Timeline():
 		self.app.update()
 
 	def __length(self):
-		return self.end_x - self.start_x
+		return self.__end_x - self.__start_x
 
 	def __tick_x(self, tick_num):
 		tick_distance = self.__length() / c.num_ticks()
-		return self.start_x + int((tick_num - c.START_TICK) * tick_distance)
+		return c.START_X + (tick_num - self.__start_tick) * tick_distance
+		# return self.__start_x + int((tick_num - c.START_TICK) * tick_distance)
 
 	def __set_zoom_factor(self):
-		self.zoom_factor = self.__length() / c.LENGTH
+		self.__zoom_factor = self.__length() / c.LENGTH
 
 	def display_line(self):
 		self.__set_zoom_factor()
 
-		self.canvas.create_line(self.start_x, c.Y, self.end_x, c.Y,  fill=c.FILL_COLOR, width=self.zoom_factor)
+		self.canvas.create_line(self.__start_x, c.Y, self.__end_x, c.Y,  fill=c.FILL_COLOR, width=self.__zoom_factor)
 
 		for tick_num in c.TICKS:
 			x = self.__tick_x(tick_num)
 			y_start = c.Y
-			y_end = y_start - (c.TICK_HEIGHT * self.zoom_factor)
+			y_end = y_start - (c.TICK_HEIGHT * self.__zoom_factor)
 
-			self.canvas.create_line(x, y_start, x, y_end, fill=c.FILL_COLOR, width = self.zoom_factor)
+			self.canvas.create_line(x, y_start, x, y_end, fill=c.FILL_COLOR, width = self.__zoom_factor)
+			self.canvas.create_text(x, y_end - 10, text=str(tick_num), font=("Times", 10, "bold"), fill='white')
 
 		self.app.update()
 
-	def zoom(self, start_tick, end_tick):
-		goal_start_x = self.__tick_x(start_tick)
-		goal_end_x   = self.__tick_x(end_tick)
+	def __eq_x(self, current_start_x, current_end_x):
+		#eq point
+		goal_start_x = c.START_X
+		goal_end_x = c.END_X
 
-		incr_start_x = (goal_start_x - self.start_x) / c.ZOOM_SMOOTHNESS
-		incr_end_x = (goal_end_x - self.end_x) / c.ZOOM_SMOOTHNESS
+		a = current_start_x - goal_start_x
+		b = goal_end_x - current_end_x
+
+		d = current_end_x - current_start_x
+		w = goal_end_x - goal_start_x
+
+		x = a / (w - d)
+
+		eq_x = a + x*d
+
+		return eq_x
+
+	def zoom(self, start_tick, end_tick):
+		self.__start_tick = start_tick
+		current_start_x = self.__tick_x(start_tick)
+		current_end_x   = self.__tick_x(end_tick)
+
+		incr_start_x = (current_start_x - self.__start_x) / c.ZOOM_SMOOTHNESS
+		incr_end_x = (current_end_x - self.__end_x) / c.ZOOM_SMOOTHNESS
+
+		print(incr_start_x, incr_end_x)
 
 		for idx in range(c.ZOOM_SMOOTHNESS):
-			self.start_x = self.start_x - incr_start_x
-			self.end_x = self.end_x - incr_end_x
+			self.__start_x = self.__start_x - incr_start_x
+			self.__end_x = self.__end_x - incr_end_x
+			print(self.__length())
 			self.clear()
 			self.display_line()
 			time.sleep(0.1)
 
 timeline = Timeline()
 timeline.display_line()
-timeline.zoom(0, 10)
-timeline.zoom(30, 40)
+timeline.zoom(56, 66)
+#timeline.zoom(30, 40)
 timeline.go()
